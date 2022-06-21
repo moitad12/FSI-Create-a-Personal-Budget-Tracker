7e68a27de419e3fac6e5d8107aba7a76c2cb80ac
Moises Tadeo <moisestadeo10@gmail.com>
2022-06-20 21:37:16 -0700

    finished creating budget tracker

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+//Ask for and calculate weekly expenses
+for(let expenseQuestion = 0; expenseQuestion < weeklyExpenseQuestions.length; expenseQuestion++) {
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[expenseQuestion]));
+console.log(answer);
+weeklyExpenses = weeklyExpenses + answer
+}
+
+//Ask for and calculate monthly expenses
+for(let expenseQuestion = 0; expenseQuestion < monthlyExpenseQuestions.length; expenseQuestion++) {
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[expenseQuestion]));
+    console.log(answer);
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+
+//Ask for and calculate annual expenses
+for(let expenseQuestion = 0; expenseQuestion < annualExpenseQuestions.length; expenseQuestion++) {
+        let answer = parseFloat(window.prompt(annualExpenseQuestions[expenseQuestion]));
+        console.log(answer);
+        annualExpenses = annualExpenses + answer
+}
